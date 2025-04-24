eab0daf55da26042f8549525a1fa9548284552b1
aghabidareh <aghabidareh@gmail.com>
2025-04-24 18:49:15 +0330

    preparing-for-new-version-import-libs

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from threading import Thread
+import torch
+from PIL import Image
+from transformers import AutoModel, AutoProcessor
+from torchvision import transforms
